8e86b66016f55598ddd6cae7404b3981c83bc6fd
Saransh Pathak <sarapat510@gmail.com>
2025-06-18 21:57:50 +0530

    Project Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,10 +40,11 @@ app.use("/api/messages", messageRoutes); // use message routes
 
 // serve frontend
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend/dist")));
+    const frontendBuildPath = path.join(__dirname, "frontend", "dist");
+    app.use(express.static(frontendBuildPath));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+        res.sendFile(path.join(frontendBuildPath, "index.html"));
     });
 }
 
